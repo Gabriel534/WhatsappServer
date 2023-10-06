4cf53fc03a2d5cec89a1e5e09672e2bb71a28e18
Gabriel Sampaio Santos <gsampaiosantos264@gmail.com>
2023-10-06 16:57:22 -0300

    login

--- creates.py
+++ creates.py
@@ -9,4 +9,19 @@ CREATE TABLE IF NOT EXISTS Usuarios
  Telefone varchar(13),
  IP varchar(12)
 );
+
+CREATE TABLE IF NOT EXISTS Contatos
+(
+Id integer PRIMARY KEY,
+Login varchar(30) not null,
+Nome varchar(30) not null
+);
+
+CREATE TABLE IF NOT EXISTS Conversas
+(
+Id integer PRIMARY KEY AUTOINCREMENT,
+IdContato integer not null,
+Horario datetime not null,
+Text varchar(500) not null
+);
 """
